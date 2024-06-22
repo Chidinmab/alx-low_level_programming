bd9b7c593e2e27a504eb49fbbe885e97a8a61bae
Chidinma <chidinmablessingpat@gmail.com>
2024-06-22 16:24:04 -0700

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
+ * main - this is the main function
  *
- * Return: Always 0 (Success)
+ * Return: always 0
  */
 int main(void)
 {
-	puts("\"Programming is like buliding a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
